8ddac310ddfa23bee6fa9066d8102e2519c2b494
Jeel Kananai <80504844+jeelkanani@users.noreply.github.com>
2025-11-07 16:17:17 +0530

    Update constants.py

--- functions/jaime_dc_online_controller/request_processor/utils/constants.py
+++ functions/jaime_dc_online_controller/request_processor/utils/constants.py
@@ -1,4 +1,6 @@
 STEP_FUNCTIONS_ARN = "STEP_FUNCTIONS_ARN"
 SUCCESS_MESSAGE = "Step functions execution started"
 ERROR_MESSAGE = "Step functions execution failed"
+
 MISSING_STEP_FUNCTIONS_ARN_MESSAGE = "STEP_FUNCTIONS_ARN is not set in environment variables."
+
